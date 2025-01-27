0421248b47fba931ca00ab42f318734a7b8e067a
thecaracter <92218072+Thecaracter@users.noreply.github.com>
2025-01-28 02:59:39 +0700

    .

--- app/Http/Controllers/Api/VideoController.php
+++ app/Http/Controllers/Api/VideoController.php
@@ -1,13 +1,15 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Video;
 use Illuminate\Http\Request;
 
-class VideoController extends Controller {
-    public function index() {
+class VideoController extends Controller
+{
+    public function index()
+    {
         $videos = Video::all();
         return response()->json([
             'status' => 'success',
