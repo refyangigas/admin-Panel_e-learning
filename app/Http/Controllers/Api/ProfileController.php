b138fd8d920e0e469c970428accb9023ef3debb9
thecaracter <92218072+Thecaracter@users.noreply.github.com>
2025-01-28 03:07:33 +0700

    .

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    public function show()
+    public function index()
     {
         $user = auth()->user();
         $profile = $user->profile;
